ce6a260614f2a69e1fc6a8afa4104824e1263054
xrohanmandal <xrohanmandal@gmail.com>
2019-12-29 07:28:46 +0000

    serc

--- first.py
+++ first.py
@@ -14,6 +14,7 @@ chrome_options.add_argument("--disable-local-storage")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 driver.get("https://x1erangel.blogspot.com/")
+
 driver.find_element_by_link_text("First Post").click()
 time.sleep(5)
 driver.find_element_by_link_text("link").click()
