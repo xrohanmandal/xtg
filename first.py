fdc8be059e1be07d779f134a820c706e3e04def8
xrohanmandal <xrohanmandal@gmail.com>
2019-12-31 04:44:56 +0000

    ronar

--- first.py
+++ first.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
+from fake_useragent import UserAgent
 import os
 import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
@@ -11,6 +15,7 @@ chrome_options.add_argument("--disable-popup-blocking")
 chrome_options.add_argument("--allow-popups-during-page-unload")
 chrome_options.add_argument("--disable-background-timer-throttling")
 chrome_options.add_argument("--disable-local-storage")
+chrome_options.add_argument(f'user-agent={userAgent}')
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 driver.get("https://x1erangel.blogspot.com/")
